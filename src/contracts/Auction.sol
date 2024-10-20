85b5999cbcd2134ebee3a0b8718aa2749ec0bea5
nameisunknown <orangekapital@gmail.com>
2024-10-20 16:40:42 +0300

    place bid function implemented

--- src/contracts/Auction.sol
+++ src/contracts/Auction.sol
@@ -4,111 +4,169 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import  "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Auction is ERC721URIStorage, ReentrancyGuard{
-  uint256 private totalItems;
-  uint8 royaltyFee;
-  address companyAcc;
-  uint256 listingPrice = 0.02 ether;
+contract Auction is ERC721URIStorage, ReentrancyGuard {
+    uint256 private totalItems;
+    uint8 royaltyFee;
+    address companyAcc;
+    uint256 listingPrice = 0.02 ether;
 
-  struct AuctionStruct{
-    string name;
-    string description;
-    string image;
-    uint256 tokenId;
-    address seller;
-    address owner;
-    address winner;
-    uint256 price;
-    bool sold;
-    bool live;
-    bool biddable;
-    uint256 bids;
-    uint duration;
+    struct AuctionStruct {
+        string name;
+        string description;
+        string image;
+        uint256 tokenId;
+        address seller;
+        address owner;
+        address winner;
+        uint256 price;
+        bool sold;
+        bool live;
+        bool biddable;
+        uint256 bids;
+        uint duration;
+    }
 
-  }
+    mapping(uint => AuctionStruct) public auctionedItem;
+    mapping(uint => bool) auctionedItemExist;
 
-  mapping(uint => AuctionStruct) public auctionedItem;
-  mapping(uint => bool) auctionedItemExist;
+    event AuctionItemCreated(
+        uint256 indexedtokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
 
-  event AuctionItemCreated(uint256 indexedtokenId, address seller,
-  address owner, uint256 price, bool sold);
+    error UnAuthorized_Entity();
 
-  error UnAuthorized_Entity();
+    constructor(uint8 _royaltyFee) ERC721("CreatCo", "CRT") {
+        royaltyFee = _royaltyFee;
+        companyAcc = msg.sender;
+    }
 
-  constructor(uint8 _royaltyFee) ERC721("CreatCo", "CRT"){
-    royaltyFee = _royaltyFee;
-    companyAcc = msg.sender;
+    function mintToken(string memory tokenURI) internal returns (bool) {
+        totalItems += 1;
+        uint256 tokenId = totalItems;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
 
-  }
+        return true;
+    }
 
-  function mintToken(string memory tokenURI) internal returns (bool) {
-    totalItems+=1;
-    uint256 tokenId = totalItems;
-    _mint(msg.sender, tokenId);
-    _setTokenURI(tokenId, tokenURI);
+    function createAuction(
+        string memory _name,
+        string memory _description,
+        string memory _image,
+        string memory _tokenURI,
+        uint _price
+    ) public payable nonReentrant {
+        require(_price > 0 ether, "This sales prices is not valid");
+        require(
+            msg.value >= listingPrice,
+            "Price must be greater than listing price"
+        );
 
-    return true;
-  }
+        require(bytes(_name).length > 0, "name cannot be empty");
+        require(bytes(_description).length > 0, "description cannot be empty");
 
-  function createAuction(string memory _name, string memory _description,
-  string memory _image, string memory _tokenURI, uint _price) public payable nonReentrant(){
-    require(_price > 0 ether, "This sales prices is not valid");
-    require(msg.value >= listingPrice, "Price must be greater than listing price");
-   
-    require(bytes(_name).length > 0, "name cannot be empty");
-    require(bytes(_description).length > 0, "description cannot be empty");
+        require(bytes(_image).length > 0, "image must be specified");
+        require(mintToken(_tokenURI), "Could not mint token");
 
-    require(bytes(_image).length > 0, "image must be specified");
-    require(mintToken(_tokenURI), "Could not mint token");
+        uint256 _tokenId = totalItems;
 
-    uint256 _tokenId = totalItems;
+        AuctionStruct memory auctions;
+        auctions.tokenId = _tokenId;
+        auctions.name = _name;
+        auctions.description = _description;
+        auctions.image = _image;
+        auctions.price = _price;
+        auctions.duration = getTimeSTamp(0, 0, 0, 0);
+        auctions.seller = msg.sender;
+        auctions.owner = msg.sender;
 
-    AuctionStruct memory auctions;
-    auctions.tokenId = _tokenId;
-    auctions.name = _name;
-    auctions.description = _description;
-    auctions.image = _image;
-    auctions.price = _price;
-    auctions.duration = getTimeSTamp(0,0,0,0);
-    auctions.seller = msg.sender;
-    auctions.owner = msg.sender;
+        auctionedItem[_tokenId] = auctions;
+        auctionedItemExist[_tokenId] = true;
 
-    auctionedItem[_tokenId] = auctions;
-    auctionedItemExist[_tokenId] = true;
+        payy(companyAcc, msg.value);
 
-    payy(companyAcc, msg.value);
+        emit AuctionItemCreated(
+            _tokenId,
+            msg.sender,
+            address(0),
+            _price,
+            false
+        );
+    }
 
-    emit AuctionItemCreated(_tokenId, msg.sender, address(0), _price, false);
-  }
+    function payy(address to, uint256 amount) internal {
+        (bool success, ) = payable(to).call{value: amount}("");
+        require(success);
+    }
 
-  function payy(address to, uint256 amount){
-  (bool success, ) = payable(to).call{value: amount}("");
-  require(success);
+    function getTimeSTamp(
+        uint256 _seconds,
+        uint256 _minutes,
+        uint256 _hour,
+        uint256 _day
+    ) public view returns (uint) {
+        return
+            block.timestamp +
+            (1 seconds * _seconds) +
+            (1 minutes * _minutes) +
+            (1 hours * _hour) +
+            (1 days * _day);
+    }
+
+    function offerAuction(
+        uint256 _tokenId,
+        bool biddable,
+        uint256 sec,
+        uint256 min,
+        uint256 hour,
+        uint256 day
+    ) public {
+        if (!auctionedItem[_tokenId].live) {
+            setApprovalForAll(address(this), true);
+            IERC721(address(this)).transferFrom(
+                msg.sender,
+                address(this),
+                _tokenId
+            );
+        }
+        auctionedItem[_tokenId].bids = 0;
+        auctionedItem[_tokenId].live = true;
+        auctionedItem[_tokenId].sold = false;
+        auctionedItem[_tokenId].biddable = biddable;
+        auctionedItem[_tokenId].duration = getTimeSTamp(sec, min, hour, day);
+    }
+
+    function buyAuctionedItem(uint256 _tokenId) public payable nonReentrant{
+      require(msg.value >= auctionedItem[_tokenId].price, "Insufficient Amount");
+      require(auctionedItem[_tokenId].duration > getTimeSTamp(0, 0, 0, 0), "Auctioned Item is not available");
+      require(!auctionedItem[_tokenId].biddable, "Auction must not be biddable");
+      AuctionStruct storage auct= auctionedItem[_tokenId];
+      address seller = auct.seller;
+      address owner = auct.owner;
+
+      auct.live = false;
+      auct.sold = true;
+      auct.bids = 0;
+      auct.duration = getTimeSTamp(0, 0, 0, 0);
+      auct.owner = msg.sender;                             
+
+      uint royalty = (msg.value * royaltyFee) / 100;
+
+      payy(owner, (msg.value - royalty));
+      payy(seller, royalty);
+
+      IERC721(address(this)).transferFrom(address(this), msg.sender, _tokenId);
+    }
+
+    function placeBid(uint256 _tokenId) public payable{
+        require(msg.value >= auctionedItem[_tokenId].price, "Insufficient Amount");
+        require(auctionedItem[_tokenId].duration > getTimeSTamp(0, 0, 0, 0), "Auctioned Item not available");
+        require(auctionedItem[_tokenId].biddable, "Auction must be biddable");
+    }
 }
-
-function getTimeSTamp(uint256 _seconds, uint256 _minutes,
-uint256 _hour, uint256 _day) public view returns (uint){
-  return 
-    block.timestamp + (1 seconds * _seconds) + (1 minutes * _minutes) +
-    (1 hours * _hour) + (1 days * _day);
-
-}
-
-function offerAuction(uint256 _tokenId, bool biddable, uint256 sec, uint256 min,
-  uint256 hour, uint256 day) public{
-      if(!!auctionedItem[_tokenId].live){
-        setApprovalForALl(address(this), true);
-        IERC721(address(this)).transferFrom(msg.sender, address, _tokenId);
-      }
-    auctionedItem[_tokenId].bids = 0;
-    auctionedItem[_tokenId].live = 0;
-    auctionedItem[_tokenId].sold = false;
-    auctionedItem[_tokenId].biddable = biddabe;
-    auctionedItem_tokenId[].durations = getTimeSTamp(sec, nin, hour, d)
-
-  }
-  
-}
-
